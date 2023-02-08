3a34cdc8d4d6e9556c215606d835a85a69f0b9a7
Jocelyn Shannon <86599636+DVJocelynShannon@users.noreply.github.com>
2023-02-08 08:40:58 +0000

    Update streamlit_app.py
    spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
